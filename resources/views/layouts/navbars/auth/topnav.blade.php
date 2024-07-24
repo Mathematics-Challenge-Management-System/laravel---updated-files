659e7ce997c5996f00720c4b49cc811533bc30a7
Arthur Asasira <asasiraarthur@gmail.com>
2024-07-24 22:51:06 +0300

    merge

--- resources/views/layouts/navbars/auth/topnav.blade.php
+++ resources/views/layouts/navbars/auth/topnav.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
+{{--                <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>--}}
                 <li class="breadcrumb-item text-sm text-white active" aria-current="page">{{ $title }}</li>
             </ol>
             @if (Auth::guard('admin')->check())
@@ -16,14 +16,16 @@
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">
                 <div class="nav-bar">
+{{--                                       <img src="/images/math.jpg" alt="Logo" style="width:80px;height:80px;">--}}
+
                     <div class="nav-links">
-                   <div> <img src="/images/math.jpg" alt="Logo" style="width:80px;height:80px;"></div>
                        <div> <a class="nav-link text-white font-weight-bold" href="{{ route('home') }}">MATHEMATICS CHALLENGE COMPETITION</a></div>
                         <div class="get-started"> <a href="{{ route('schools-performance') }}"></a></div>
                     </div>
                 </div>
                 <ul class="navbar-nav justify-content-end">
                     <li class="nav-item d-flex align-items-center">
+                        @if (Auth::guard('admin')->check())
                         <form role="form" method="post" action="{{ route('logout') }}" id="logout-form">
                             @csrf
                             <a href="{{ route('logout') }}"
@@ -33,6 +35,7 @@
                                 <span class="d-sm-inline d-none">Log out</span>
                             </a>
                         </form>
+                        @endif
                     </li>
                 </ul>
             </div>
